fa92fe139eb673c848a1b56fedc75fd7ba70ea8f
william <williammbitencourtmesquita@gmail.com>
2026-03-22 18:03:56 -0300

    Added button styles data class

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -15,14 +15,18 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.calculator.ui.theme.CalculatorTheme
@@ -34,55 +38,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CalculatorTheme {
-                Greeting()
+                GreetingPreview()
             }
         }
     }
 }
-
-@Composable
-fun Greeting() {
-    GreetingPreview()
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalculatorTheme {
-
-        val screenColor    = Color(0xFF000000)
-        val bottomBoxColor = Color(0xFF444444)
-        val primaryColor   = Color(0xFF465C91)
-
-        Column()
-        {
-            // Just a header
-            Box(modifier = Modifier
-                .background(primaryColor)
-                .height(20.dp)
-                .fillMaxWidth()
-            ){}
-
-
-            Box(
-                modifier = Modifier
-                    .background(screenColor) // Black background
-                    .height(200.dp)
-                    .fillMaxWidth()
-            )
-            {
-
-                Text(text = "0", color = Color(0xFFFFFFFF), fontSize = 100.sp)
-            }
-
-            TouchPad(
-                primaryColor,
-                bottomBoxColor
-            )
-        }
-    }
-}
-
 
 enum class DisplayOrder(val index: Int){
     FIRST(0),
@@ -91,8 +51,59 @@ enum class DisplayOrder(val index: Int){
     FOURTH(3)
 }
 
+data class ButtonStyles(
+    val color: Color,
+    val shape: Shape,
+    val fontSize: TextUnit
+)
+
+@Preview(showBackground = true)
 @Composable
-fun TouchPad(buttonsColor: Color, backgroundColor : Color){
+fun GreetingPreview() {
+    val screenColor        = Color(0xFF000000)
+    val touchPadBackground = Color(0xFF444444)
+    val primaryColor       = Color(0xFF465C91)
+
+
+    val buttonStyles = ButtonStyles(
+        primaryColor,
+        RoundedCornerShape(50.dp),
+        50.sp
+    )
+
+    Column(modifier = Modifier
+        .background(primaryColor)
+        .padding(top = 35.dp)
+    ){
+
+        Screen(screenColor)
+
+        TouchPad(
+            buttonStyles,
+            touchPadBackground
+        )
+    }
+}
+
+@Composable
+fun Screen(screenColor : Color){
+    Box(
+        modifier = Modifier
+            .background(screenColor)
+            .height(200.dp)
+            .fillMaxWidth(),
+        contentAlignment = Alignment.TopEnd
+    ){
+        Text(
+            text = "0",
+            color = Color(0xFFFFFFFF),
+            fontSize = 70.sp
+        )
+    }
+}
+
+@Composable
+fun TouchPad(buttonsStyles: ButtonStyles, backgroundColor : Color){
     val buttonsText = arrayOf(
         arrayOf("7", "8", "9", "/"),
         arrayOf("4", "5", "6", "*"),
@@ -111,33 +122,33 @@ fun TouchPad(buttonsColor: Color, backgroundColor : Color){
         )
         {
             // matrix[row][col]
-            CalculatorRow(buttonsColor,buttonsText[DisplayOrder.FIRST.index])
-            CalculatorRow(buttonsColor,buttonsText[DisplayOrder.SECOND.index])
-            CalculatorRow(buttonsColor,buttonsText[DisplayOrder.THIRD.index])
-            CalculatorRow(buttonsColor,buttonsText[DisplayOrder.FOURTH.index])
+            CalculatorRow(buttonsStyles,buttonsText[DisplayOrder.FIRST.index])
+            CalculatorRow(buttonsStyles,buttonsText[DisplayOrder.SECOND.index])
+            CalculatorRow(buttonsStyles,buttonsText[DisplayOrder.THIRD.index])
+            CalculatorRow(buttonsStyles,buttonsText[DisplayOrder.FOURTH.index])
         }
     }
 }
 
 @Composable
-fun ColumnScope.CalculatorRow(buttonsColor: Color, texts: Array<String>){
+fun ColumnScope.CalculatorRow(buttonsStyles: ButtonStyles, texts: Array<String>){
     Row(modifier = Modifier
         .fillMaxWidth()
         .weight(1f),
         horizontalArrangement = Arrangement.SpaceBetween
     )
     {
-        CalculatorButton(buttonsColor, texts[DisplayOrder.FIRST.index])
-        CalculatorButton(buttonsColor, texts[DisplayOrder.SECOND.index])
-        CalculatorButton(buttonsColor, texts[DisplayOrder.THIRD.index])
-        CalculatorButton(buttonsColor, texts[DisplayOrder.FOURTH.index])
+        CalculatorButton(buttonsStyles, texts[DisplayOrder.FIRST.index])
+        CalculatorButton(buttonsStyles, texts[DisplayOrder.SECOND.index])
+        CalculatorButton(buttonsStyles, texts[DisplayOrder.THIRD.index])
+        CalculatorButton(buttonsStyles, texts[DisplayOrder.FOURTH.index])
     }
 }
 
 
 @Composable
 fun RowScope.CalculatorButton(
-    buttonColor: Color,
+    buttonStyle: ButtonStyles,
     text: String,
     onClick: () -> Unit = {}
 ) {
@@ -146,11 +157,14 @@ fun RowScope.CalculatorButton(
         modifier = Modifier
             .weight(1f)
             .fillMaxHeight(),
-        shape = RoundedCornerShape(50.dp),
+        shape = buttonStyle.shape,
         colors = ButtonDefaults.buttonColors(
-            containerColor = buttonColor
+            containerColor = buttonStyle.color
         )
     ) {
-        Text(text = text, fontSize = 50.sp)
+        Text(
+            text = text,
+            fontSize = buttonStyle.fontSize
+        )
     }
 }
